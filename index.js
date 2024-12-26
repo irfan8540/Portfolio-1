fd54c941b0e6331bec634adc0f76598063387425
Irfan Ahmad <irfanhahid3395@gmail.com>
2024-12-26 20:27:01 +0530

    Portfolio code

---
+++ index.js
@@ -0,0 +1,9 @@
+// Smooth Scrolling
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
